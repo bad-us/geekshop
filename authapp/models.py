495f546b2269b4a5c780e6b8f369c03974faab67
Bad <bad-us@rambler.ru>
2021-08-05 16:56:19 +0300

    Rerplace

--- authapp/models.py
+++ authapp/models.py
@@ -4,11 +4,13 @@ import pytz
 from django.conf import settings
 from django.db import models
 from django.contrib.auth.models import AbstractUser
+from django.db.models.signals import post_save
+from django.dispatch import receiver
 
 
 class ShopUser(AbstractUser):
     avatar = models.ImageField(upload_to='users_avatars', blank=True)
-    age = models.PositiveIntegerField(verbose_name = 'возраст', default=18)
+    age = models.PositiveIntegerField(verbose_name='возраст', default=18)
 
     activation_key = models.CharField(max_length=128, blank=True)
     activation_key_created = models.DateTimeField(auto_now_add=True, null=True, blank=True)
@@ -17,3 +19,28 @@ class ShopUser(AbstractUser):
         if datetime.now(pytz.timezone(settings.TIME_ZONE)) < (self.activation_key_created + timedelta(hours=48)):
             return False
         return True
+
+
+class ShopUserProfile(models.Model):
+    MALE = 'M'
+    FEMALE = 'W'
+
+    GENDER_CHOICES = (
+        (MALE, 'М'),
+        (FEMALE, 'Ж'),
+    )
+
+    user = models.OneToOneField(ShopUser, unique=True, null=False, db_index=True, on_delete=models.CASCADE)
+    tagline = models.CharField(verbose_name='tags', max_length=128, blank=True)
+    aboutMe = models.TextField(verbose_name='about me', max_length=512, blank=True)
+    gender = models.CharField(max_length=1, choices=GENDER_CHOICES, blank=True, verbose_name='gender')
+
+
+    @receiver(post_save, sender=ShopUser)
+    def create_user_profile(sender, instance, created, **kwargs):
+        if created:
+            ShopUserProfile.objects.create(user=instance)
+
+    @receiver(post_save, sender=ShopUser)
+    def save_user_profile(sender, instance, **kwargs):
+        instance.shopuserprofile.save()
