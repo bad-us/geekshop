b700c2f644aed5d7c4132703930dfb78da8b896f
Bad <bad-us@rambler.ru>
2021-08-05 18:17:12 +0300

    Center

--- authapp/forms.py
+++ authapp/forms.py
@@ -3,7 +3,7 @@ import random
 
 from django import forms
 from django.contrib.auth.forms import AuthenticationForm, UserCreationForm
-from .models import ShopUser
+from .models import ShopUser, ShopUserProfile
 
 from django.contrib.auth.forms import UserChangeForm
 
@@ -12,24 +12,24 @@ class ShopUserLoginForm(AuthenticationForm):
     class Meta:
         model = ShopUser
         fields = ('username', 'password')
-    
+
     def __init__(self, *args, **kwargs):
         super(ShopUserLoginForm, self).__init__(*args, **kwargs)
         for field_name, field in self.fields.items():
             field.widget.attrs['class'] = 'form-control'
-            
-            
+
+
 class ShopUserRegisterForm(UserCreationForm):
     class Meta:
         model = ShopUser
         fields = ('username', 'first_name', 'password1', 'password2', 'email', 'age', 'avatar')
-    
+
     def __init__(self, *args, **kwargs):
         super(ShopUserRegisterForm, self).__init__(*args, **kwargs)
         for field_name, field in self.fields.items():
             field.widget.attrs['class'] = 'form-control'
             field.help_text = ''
-    
+
     def clean_age(self):
         data = self.cleaned_data['age']
         if data < 18:
@@ -45,13 +45,12 @@ class ShopUserRegisterForm(UserCreationForm):
         user.save()
         return user
 
-        
 
 class ShopUserEditForm(UserChangeForm):
     class Meta:
         model = ShopUser
         fields = ('username', 'first_name', 'email', 'age', 'avatar', 'password')
-    
+
     def __init__(self, *args, **kwargs):
         super(ShopUserEditForm, self).__init__(*args, **kwargs)
         for field_name, field in self.fields.items():
@@ -59,10 +58,22 @@ class ShopUserEditForm(UserChangeForm):
             field.help_text = ''
             if field_name == 'password':
                 field.widget = forms.HiddenInput()
-    
+
     def clean_age(self):
         data = self.cleaned_data['age']
         if data < 18:
             raise forms.ValidationError("Вы слишком молоды!")
 
         return data
+
+
+class ShopUserProfileEdit(forms.ModelForm):
+    class Meta:
+        model = ShopUserProfile
+        fields = ('tagline', 'aboutMe', 'gender',)
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        for field_name, field in self.fields.items():
+            field.widget.attrs['class'] = 'form-control'
+            field.help_text = ''
